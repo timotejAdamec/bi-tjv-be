527f084c636c604ff5e8bd6ed9b6dbfc4b641042
Timotej Adamec <adametim@fit.cvut.cz>
2022-10-23 11:14:50 +0200

    master: init

---
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/SemesterProjectApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.adametim.bi_tjv.semester_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesterProjectApplication.class, args);
+	}
+
+}
