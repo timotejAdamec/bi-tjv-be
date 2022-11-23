c0f8fc54d7c03ae4deebc56d27d35a39269fb854
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 15:50:22 +0100

    clientDto

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ClientDto.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ClientDto.java
@@ -4,4 +4,14 @@ package cz.cvut.fit.adametim.bi_tjv.semester_project.api.model;
 public final class ClientDto {
 
     private Long clientId;
+
+    private String name;
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
