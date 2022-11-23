1586779202d2ae85e384d582d6153f945a8cd5a3
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 21:40:10 +0100

    Added rich business operation

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/ProjectService.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/ProjectService.java
@@ -1,14 +1,44 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.business;
 
+import cz.cvut.fit.adametim.bi_tjv.semester_project.dao.ManagerRepository;
 import cz.cvut.fit.adametim.bi_tjv.semester_project.dao.ProjectRepository;
-import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Client;
+import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Manager;
 import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Project;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Optional;
 
 @Service
-public final class ProjectService extends AbstractCrudService<Project, Long> {
+public class ProjectService extends AbstractCrudService<Project, Long> {
 
-    public ProjectService(ProjectRepository repository) {
+    private final ManagerRepository managerRepository;
+
+    public ProjectService(ProjectRepository repository,
+                          ManagerRepository clientRepository) {
         super(repository);
+        this.managerRepository = clientRepository;
+    }
+
+    /**
+     * Sets as {@code Project.currentManager} the manager with managerId {@code managerId} in all projects
+     * that have this manager in {@code Project.managersHistory}.
+     *
+     * @param managerId of the manager that should take over the projects.
+     */
+    @Transactional
+    void takeOverProjectsOfColleagues(Long managerId) {
+        Optional<Manager> managerResult = managerRepository.findById(managerId);
+        if (managerResult.isEmpty()) {
+            throw new EntityStateException("Manager with managerId " + managerId + " does not exist!");
+        }
+        Manager manager = managerResult.get();
+
+        Collection<Project> colleagueProjects = ((ProjectRepository) repository).getColleagueProjects(manager);
+
+        for (Project colleagueProject: colleagueProjects) {
+            colleagueProject.setCurrentManager(manager);
+        }
     }
 }
--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/ProjectRepository.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/ProjectRepository.java
@@ -1,9 +1,15 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.dao;
 
+import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Manager;
 import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query(value = "SELECT p FROM Project p WHERE :manager IN p.managersHistory")
+    Collection<Project> getColleagueProjects(Manager manager);
 }
