c633177d3d45467c3322fc532790a6d8b6c27ebf
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 15:00:28 +0100

    repository are jpa

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/ClientRepository.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/ClientRepository.java
@@ -1,7 +1,9 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.dao;
 
+import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/ManagerRepository.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/ManagerRepository.java
@@ -1,7 +1,10 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.dao;
 
+import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Client;
+import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ManagerRepository {
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
 }
