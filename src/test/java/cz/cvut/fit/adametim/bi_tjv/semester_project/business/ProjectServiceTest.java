6e444fa8efc5ebcb255756ebbc684fb4e2adfac8
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 22:48:44 +0100

    added getColleagues query

---
+++ src/test/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/ProjectServiceTest.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.adametim.bi_tjv.semester_project.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void takeOverProjectsOfColleagues() {
+        // TODO
+    }
+}
