fe86a2c88dc75e9b35b6955c8b00130f844001a0
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 15:13:49 +0100

    init controllers and dtos

---
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ManagerDto.java
@@ -0,0 +1,4 @@
+package cz.cvut.fit.adametim.bi_tjv.semester_project.api.model;
+
+public final class ManagerDto {
+}
--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/ClientService.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/ClientService.java
@@ -1,12 +1,14 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.business;
 
 import cz.cvut.fit.adametim.bi_tjv.semester_project.dao.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class ClientService {
     private final ClientRepository repository;
 
+    @Autowired
     public ClientService(ClientRepository repository) {
         this.repository = repository;
     }
