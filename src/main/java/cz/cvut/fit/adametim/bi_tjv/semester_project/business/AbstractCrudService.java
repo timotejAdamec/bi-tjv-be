131343cc88d625138721f79726fe598623456ac1
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 19:34:26 +0100

    Added AbstractCrudService

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/AbstractCrudService.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/AbstractCrudService.java
@@ -1,2 +1,60 @@
-package cz.cvut.fit.adametim.bi_tjv.semester_project.business;public class AbstractCrudService {
+package cz.cvut.fit.adametim.bi_tjv.semester_project.business;
+
+import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.DomainEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * Common superclass for business logic of all entities supporting operations Create, Read, Update, Delete.
+ *
+ * @param <K> Type of (primary) key.
+ * @param <E> Type of entity
+ */
+public abstract class AbstractCrudService<E extends DomainEntity<K>, K> {
+    /**
+     * Reference to data (persistence) layer.
+     */
+    protected final CrudRepository<E, K> repository;
+
+    protected AbstractCrudService(CrudRepository<E, K> repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Attempts to store a new entity. Throws exception if an entity with the same key is already stored.
+     *
+     * @param entity entity to be stored
+     * @throws EntityStateException if an entity with the same key is already stored
+     */
+    public E create(E entity) throws EntityStateException {
+        if (repository.existsById(entity.getId()))
+            throw new EntityStateException(entity);
+        return repository.save(entity); //delegate call to data layer
+    }
+
+    public Optional<E> readById(K id) {
+        return repository.findById(id);
+    }
+
+    public Iterable<E> readAll() {
+        return repository.findAll();
+    }
+
+    /**
+     * Attempts to replace an already stored entity.
+     *
+     * @param entity the new state of the entity to be updated; the instance must contain a key value
+     * @throws EntityStateException if the entity cannot be found
+     */
+    public E update(E entity) throws EntityStateException {
+        if (repository.existsById(entity.getId()))
+            return repository.save(entity);
+        else
+            throw new EntityStateException(entity);
+    }
+
+    public void deleteById(K id) {
+        repository.deleteById(id);
+    }
 }
--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/EntityStateException.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/business/EntityStateException.java
@@ -1,2 +1,17 @@
-package cz.cvut.fit.adametim.bi_tjv.semester_project.business;public class EntityStateException {
+package cz.cvut.fit.adametim.bi_tjv.semester_project.business;
+
+/**
+ * An exception indicating problem related to existence of an entity.
+ */
+public class EntityStateException extends RuntimeException {
+    public EntityStateException() {
+    }
+
+    public <E> EntityStateException(E entity) {
+        super("Illegal state of entity " + entity);
+    }
+
+    public EntityStateException(String s) {
+        super(s);
+    }
 }
