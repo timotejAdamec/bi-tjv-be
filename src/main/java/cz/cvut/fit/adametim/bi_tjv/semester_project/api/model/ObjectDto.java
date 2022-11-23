d0a4bbe0ecb3668a94c8a3d376eef86efb7c92da
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 15:49:06 +0100

    objectDto

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ObjectDto.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ObjectDto.java
@@ -3,4 +3,14 @@ package cz.cvut.fit.adametim.bi_tjv.semester_project.api.model;
 public final class ObjectDto {
 
     private Long objectId;
+
+    private Long projectId;
+
+    public Long getObjectId() {
+        return objectId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
 }
