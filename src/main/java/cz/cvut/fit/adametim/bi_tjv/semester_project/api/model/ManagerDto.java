70c3897409386496c0ec251db864418705bf29b9
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 15:49:48 +0100

    managerDto

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ManagerDto.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ManagerDto.java
@@ -3,4 +3,20 @@ package cz.cvut.fit.adametim.bi_tjv.semester_project.api.model;
 public final class ManagerDto {
 
     private Long managerId;
+
+    private String name;
+
+    private String phoneNumber;
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
