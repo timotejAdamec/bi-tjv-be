6539c9b51487dacbe98c78b739b2b23def73f84d
Timotej Adamec <adametim@fit.cvut.cz>
2022-11-23 15:40:06 +0100

    added id to Dtos

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ClientDto.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ClientDto.java
@@ -2,4 +2,6 @@ package cz.cvut.fit.adametim.bi_tjv.semester_project.api.model;
 
 // JsonView
 public final class ClientDto {
+
+    private Long clientId;
 }
--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ProjectDto.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/api/model/ProjectDto.java
@@ -1,4 +1,6 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.api.model;
 
 public final class ProjectDto {
+
+    private Long projectId;
 }
