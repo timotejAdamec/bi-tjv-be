ef49140d09ac9c9baa1b71908dde9ffa3b59d872
Timotej Adamec <adametim@fit.cvut.cz>
2022-12-09 14:53:34 +0100

    Change repositores from JPA to Crud

--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/jpa/ClientRepository.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/jpa/ClientRepository.java
@@ -1,9 +1,9 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.dao.jpa;
 
 import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends CrudRepository<Client, Long> {
 }
--- src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/jpa/ObjectRepository.java
+++ src/main/java/cz/cvut/fit/adametim/bi_tjv/semester_project/dao/jpa/ObjectRepository.java
@@ -1,9 +1,9 @@
 package cz.cvut.fit.adametim.bi_tjv.semester_project.dao.jpa;
 
 import cz.cvut.fit.adametim.bi_tjv.semester_project.domain.Object;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ObjectRepository extends JpaRepository<Object, Long> {
+public interface ObjectRepository extends CrudRepository<Object, Long> {
 }
